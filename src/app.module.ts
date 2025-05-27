49f3585fbd89781452de2288f8373c8816567292
Zayed Hossain <zayedhossain120@gmail.com>
2025-05-27 21:39:41 +0600

    working on auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PrismaModule],
 })
 export class AppModule {}
